5d5e51523c00ebd58907fc0542df8956eef2bed1
tomqrlz <myslajekt@gmail.com>
2021-05-31 19:37:12 +0200

    moves upgrade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import WIDTH, HEIGHT, ROWS, COLS
+from constants import SQUARE_SIZE, WIDTH, HEIGHT, ROWS, COLS
 from board import Board
 
 '''
@@ -18,21 +18,28 @@ def clicked_pos(position):
     return int(position[0]/(WIDTH/ROWS)), int(position[1]/(HEIGHT/COLS))
 
 def turn(x, y, piece_clicked):
-    if piece_clicked: # We want to click an empty space
-        if board.is_piece(x, y) == False:
-            print('Moved')
-            board.move_piece(*piece_clicked, x, y)
-            return None
-        else:
-            print('Not Moved')
-            return piece_clicked
-    else:
+    if not piece_clicked: # We want to click an empty space
         if board.is_piece(x, y): # We want to click a piece
             print('Piece clicked')
             return (x, y)
         else:
             print('Piece not clicked')
             return None
+    else:
+        if board.is_piece(x, y):
+            if (piece_clicked[0] == x and piece_clicked[1] == y):
+                print('Piece unclicked')
+                return None        
+            else:
+                print('Piece clicked')
+                return (x, y)
+        elif board.is_piece(x, y) == False:
+            print('Moved')
+            board.move_piece(*piece_clicked, x, y)
+            return None
+        else:
+            print('Not Moved')
+            return piece_clicked
 
 if __name__ == '__main__':
     run = True
@@ -56,6 +63,8 @@ if __name__ == '__main__':
 
         board.draw_board(WINDOW)
         board.draw_pieces(WINDOW)
+        if(piece_clicked):
+            pygame.draw.circle(WINDOW, (255,255,255), (x*SQUARE_SIZE+SQUARE_SIZE/2, y*SQUARE_SIZE+SQUARE_SIZE/2), 10)
         pygame.display.update()
 
     pygame.quit()
